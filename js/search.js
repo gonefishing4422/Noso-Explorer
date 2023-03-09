789aa847b7301a08a8a28d9a05b7d33710b9f67b
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-03-08 19:21:09 -0600

    update

--- js/search.js
+++ js/search.js
@@ -18,7 +18,7 @@ $(function() {
 	});
 
 	function displayBlockNumberResults(response) {
-		var tableHtml = '<table class="table">';
+		var tableHtml = '<table class="styled-table">';
 		tableHtml += '<tr><th>Timestamp</th><td>' + response.data.unixTimestamp + '</td></tr>';
 		tableHtml += '<tbody><tr><th>Block Number</th><td>' + response.data.blockNumber +'</td></tr>';
 		tableHtml += '<tr><th>Block Hash</th><td>' + response.data.blockHash + '</td></tr>';
@@ -27,14 +27,17 @@ $(function() {
 		tableHtml += '<tr><th>Time Start</th><td>' + response.data.timeStart + '</td></tr>';
 		tableHtml += '<tr><th>Time End</th><td>' + response.data.timeEnd + '</td></tr>';
 		tableHtml += '<tr><th>Time Total</th><td>' + response.data.timeTotal + '</td></tr>';
-		tableHtml += '<tr><th>Masternodes</th><td>' + response.data.mnsQ + '</td></tr>';
+		//tableHtml += '<tr><th>Masternode Count</th><td>' + response.data.mnsQ + '</td></tr>';
+		tableHtml += '<tr><th>Masternode Count</th><td><a href="explorer.html?uristring=' + response.data.mnsQ + '">' + response.data.mnsQ + '</a></td></tr>';
 		tableHtml += '<tr><th>Masternode Reward</th><td>' + response.data.mns.mnsRewardInPedro + '</td></tr>';
-		tableHtml += '<tr><th><a href="masternodes.html">Masternode Addresses</a></th><td>' + response.data.mns.mnsAddresses + '</td></tr>';
-		tableHtml += '<tr><th>PoS</th><td>' + response.data.posQ + '</td></tr>';
+		//tableHtml += '<tr><th><a href="masternodes.html">Masternode Addresses</a></th><td>' + response.data.mns.mnsAddresses + '</td></tr>';
+	// table for Masternode and PoS addresses needs created
+		//tableHtml += '<tr><th>PoS Count</th><td>' + response.data.posQ + '</td></tr>';
+		tableHtml += '<tr><th>PoS Count</th><td><a href="explorer.html?uristring=' + response.data.posQ + '">' + response.data.posQ + '</a></td></tr>';	
 		tableHtml += '<tr><th>PoS Reward</th><td>' + response.data.pos.posRewardInPedro + '</td></tr>';
-		tableHtml += '<tr><th>PoS Addresses</th><td>' + response.data.pos.posAddresses + '</td></tr>';
+		//tableHtml += '<tr><th>PoS Addresses</th><td>' + response.data.pos.posAddresses + '</td></tr>';
 		tableHtml += '<tr><th>PoPW Reward</th><td>' + response.data.powRewardInPedro + '</td></tr>';
-		tableHtml += '<tr><th>Unique Address</th><td>' + response.data.uniqueAddresesList + '</td></tr>';
+		//tableHtml += '<tr><th>Unique Address</th><td>' + response.data.uniqueAddresesList + '</td></tr>';
 		tableHtml += '<tr><th>Solution</th><td>' + response.data.solution + '</td></tr>';
 		tableHtml += '<tr><th>Target Hash</th><td>' + response.data.targetHash + '</td></tr>';
 		tableHtml += '</tbody></table>';
