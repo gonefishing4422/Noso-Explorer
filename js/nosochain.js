e04787eadd1228d0435afada497a45158704a854
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-03-12 03:11:47 -0500

    update

---
+++ js/nosochain.js
@@ -0,0 +1,24 @@
+		fetch('https://nosostats.com:49443/api/last143V2')
+		.then(response => response.json())
+		.then(data => {
+			const tableBody = document.querySelector('#nosochain tbody');
+
+			data.forEach(item => {
+				const row = document.createElement('tr');
+				row.innerHTML = `
+					<td class="priority-1"><a href="blocklookup.html?blocknumber=${item[0]}">${item[0]}</a></td>
+					<td class="priority-2">${item[1]}</td>
+					<td class="priority-2">${item[2]}</td>
+					<td class="priority-2">${item[3]}</td>
+					<td class="priority-3">${item[4]}</td>
+					<td class="priority-4">${item[5]}</td>
+					<td class="priority-5">${item[6]}</td>
+					<td class="priority-6">${item[7]}</td>
+					<td class="priority-7">${item[8]}</td>
+					<td class="priority-8">${item[9]}</td>
+					<td class="priority-8">${item[10]}</td>
+				`;
+				tableBody.appendChild(row);
+			});
+		})
+		.catch(error => console.error(error));
