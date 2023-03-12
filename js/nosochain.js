d3c2463d45bba6607481c7794d12abb4e28389c4
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-03-12 03:45:56 -0500

    update

--- js/nosochain.js
+++ js/nosochain.js
@@ -1,24 +1,55 @@
-		fetch('https://nosostats.com:49443/api/last143V2')
-		.then(response => response.json())
-		.then(data => {
-			const tableBody = document.querySelector('#nosochain tbody');
+fetch('https://nosostats.com:49443/api/last143V2')
+	.then(response => response.json())
+	.then(data => {
+		const tableBody = document.querySelector('#nosochain tbody');
 
-			data.forEach(item => {
-				const row = document.createElement('tr');
-				row.innerHTML = `
-					<td class="priority-1"><a href="blocklookup.html?blocknumber=${item[0]}">${item[0]}</a></td>
-					<td class="priority-2">${item[1]}</td>
-					<td class="priority-2">${item[2]}</td>
-					<td class="priority-2">${item[3]}</td>
-					<td class="priority-3">${item[4]}</td>
-					<td class="priority-4">${item[5]}</td>
-					<td class="priority-5">${item[6]}</td>
-					<td class="priority-6">${item[7]}</td>
-					<td class="priority-7">${item[8]}</td>
-					<td class="priority-8">${item[9]}</td>
-					<td class="priority-8">${item[10]}</td>
-				`;
-				tableBody.appendChild(row);
-			});
-		})
-		.catch(error => console.error(error));
+		data.forEach(item => {
+			const row = document.createElement('tr');
+			row.innerHTML = `
+				<td class="priority-1"><a href="blocklookup.html?blocknumber=${item[0]}">${item[0]}</a></td>
+				<td class="priority-2">${item[1]}</td>
+				<td class="priority-2">${item[2]}</td>
+				<td class="priority-2">${item[3]}</td>
+				<td class="priority-3">${item[4]}</td>
+				<td class="priority-4">${item[5]}</td>
+				<td class="priority-5">${item[6]}</td>
+				<td class="priority-6">${item[7]}</td>
+				<td class="priority-7">${item[8]}</td>
+				<td class="priority-8">${item[9]}</td>
+				<td class="priority-8">${item[10]}</td>
+			`;
+			tableBody.appendChild(row);
+		});
+	})
+	.catch(error => console.error(error));
+
+// Update the table every 10 seconds
+setInterval(() => {
+    fetch('https://nosostats.com:49443/api/last143V2')
+	.then(response => response.json())
+	.then(data => {
+		const tableBody = document.querySelector('#nosochain tbody');
+
+		// Clear existing rows
+		tableBody.innerHTML = '';
+
+		data.forEach(item => {
+			const row = document.createElement('tr');
+			row.innerHTML = `
+				<td class="priority-1"><a href="blocklookup.html?blocknumber=${item[0]}">${item[0]}</a></td>
+				<td class="priority-2">${item[1]}</td>
+				<td class="priority-2">${item[2]}</td>
+				<td class="priority-2">${item[3]}</td>
+				<td class="priority-3">${item[4]}</td>
+				<td class="priority-4">${item[5]}</td>
+				<td class="priority-5">${item[6]}</td>
+				<td class="priority-6">${item[7]}</td>
+				<td class="priority-7">${item[8]}</td>
+				<td class="priority-8">${item[9]}</td>
+				<td class="priority-8">${item[10]}</td>
+			`;
+			tableBody.appendChild(row);
+		});
+	})
+	.catch(error => console.error(error));
+}, 10000);
--- js/nosonodes.js
+++ js/nosonodes.js
@@ -15,9 +15,9 @@ fetch(url)
 
     // Build the HTML table rows
     let tableRows = '';
-    tableRows += `<tr><th class="priority-1">Node Address</th><th class="priority-2">Node IP Address</th><th class="priority-3">Node Listening Port</th><th class="priority-4">Country</th><th class="priority-5">Region</th><th class="priority-6">Node Uptime</th><th class="priority-7">Latitude</th><th class="priority-8">Longitude</th></tr>`;
+    tableRows += `<tr><th class="priority-1">Node Address</th><th class="priority-2">Node IP Address</th><th class="priority-3">Node Listening Port</th><th class="priority-2">Country</th><th class="priority-5">Region</th><th class="priority-6">Node Uptime</th><th class="priority-7">Latitude</th><th class="priority-8">Longitude</th></tr>`;
     for (let line of uniqueLines) {
-      tableRows += `<tr><td class="priority-1"><a href="addresslookup.html?addresslookup=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-2">${line.nodeIp}</td><td class="priority-3">${line.nodePort}</td><td class="priority-4">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuouslyActive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
+      tableRows += `<tr><td class="priority-1"><a href="addresslookup.html?addresslookup=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-2">${line.nodeIp}</td><td class="priority-3">${line.nodePort}</td><td class="priority-2">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuouslyActive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
     }
 
     // Insert the table rows into the HTML table body
@@ -26,9 +26,9 @@ fetch(url)
   .catch(error => console.error(error)); // Handle errors  
   
 
-    tableRows += `<tr><th class="priority-1">Node Address</th><th class="priority-2">Node IP Address</th><th class="priority-3">Node Listening Port</th><th class="priority-4">Country</th><th class="priority-5">Region</th><th class="priority-6">Node Uptime</th><th class="priority-7">Latitude</th><th class="priority-8">Longitude</th></tr>`;
+    tableRows += `<tr><th class="priority-1">Node Address</th><th class="priority-2">Node IP Address</th><th class="priority-3">Node Listening Port</th><th class="priority-2">Country</th><th class="priority-5">Region</th><th class="priority-6">Node Uptime</th><th class="priority-7">Latitude</th><th class="priority-8">Longitude</th></tr>`;
     for (let line of uniqueLines) {
-      tableRows += `<tr><td class="priority-1"><a href="addresslookup.html?addresslookup=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-2">${line.nodeIp}</td><td class="priority-3">${line.nodePort}</td><td class="priority-4">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuouslyActive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
+      tableRows += `<tr><td class="priority-1"><a href="addresslookup.html?addresslookup=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-2">${line.nodeIp}</td><td class="priority-3">${line.nodePort}</td><td class="priority-2">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuouslyActive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
     }
     
    
