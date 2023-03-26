612176d4db58d8fcb2ac94cf01ba9a89123ed266
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-03-25 22:51:07 -0500

    fix addresslookup incorrect links

--- js/addresslookup1.js
+++ js/addresslookup1.js
@@ -25,29 +25,58 @@ function submitForm() {
   // Show the spinner while waiting for the API response
   spinner.style.display = 'inline-block';
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      // Clear the existing content of the table body
-      tableBody.innerHTML = '';
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Clear the existing content of the table body
+    tableBody.innerHTML = '';
 
-      // Loop through the data and add a new row for each property
-      for (const [key, value] of Object.entries(data)) {
+    // Loop through the data and add a new row for each property
+    for (const [key, value] of Object.entries(data)) {
+      const row = document.createElement('tr');
+      const propCell = document.createElement('td');
+      const valueCell = document.createElement('td');
+
+      propCell.textContent = key;
+
+      if (key === 'addresscreatedatblock' || key === 'NosoAddress') {
+        // Create a hyperlink to the desired page for the "addresscreatedatblock" and "NosoAddress" fields
+        const link = document.createElement('a');
+        link.textContent = value;
+        link.href = (key === 'addresscreatedatblock') ? `blocklookup.html?blocknumber=${value}` : `addresshistory.html?input-field=${encodeURIComponent(value)}`;
+        valueCell.appendChild(link);
+      } else if (/^\d{1,10}$/.test(value)) {
+        // Check if the value is a number with 10 or fewer digits
+        // If so, create a hyperlink to "blocklookup.html"
+        const link = document.createElement('a');
+        link.textContent = value;
+        link.href = `blocklookup.html?blocknumber=${value}`;
+        valueCell.appendChild(link);
+      } else {
+        // If not, just display the value
+        valueCell.textContent = value;
+      }
+
+      row.appendChild(propCell);
+      row.appendChild(valueCell);
+      tableBody.appendChild(row);
+    }
+
+    // Add the missing fields from the "analyzed" object if present
+    if (data.analyzed) {
+      for (const [key, value] of Object.entries(data.analyzed)) {
         const row = document.createElement('tr');
         const propCell = document.createElement('td');
         const valueCell = document.createElement('td');
 
         propCell.textContent = key;
 
-        // Check if the value is a number with 10 or fewer digits
         if (/^\d{1,10}$/.test(value)) {
-          // If so, create a hyperlink to "https://example.com/number/{value}"
           const link = document.createElement('a');
           link.textContent = value;
           link.href = `blocklookup.html?blocknumber=${value}`;
           valueCell.appendChild(link);
         } else {
-          // If not, just display the value
           valueCell.textContent = value;
         }
 
@@ -55,27 +84,29 @@ function submitForm() {
         row.appendChild(valueCell);
         tableBody.appendChild(row);
       }
+    }
 
-// Create a custom row for NosoAddress
-const nosoAddressRow = document.createElement('tr');
-const nosoAddressPropCell = document.createElement('td');
-const nosoAddressValueCell = document.createElement('td');
+    // Create a custom row for NosoAddress
+    const nosoAddressRow = document.createElement('tr');
+    const nosoAddressPropCell = document.createElement('td');
+    const nosoAddressValueCell = document.createElement('td');
 
-nosoAddressPropCell.textContent = 'NosoAddress';
+    nosoAddressPropCell.textContent = 'NosoAddress';
 
-// Create a hyperlink to "addresshistory.html" for the NosoAddress value
-const link = document.createElement('a');
-link.href = `addresshistory.html?input-field=${encodeURIComponent(inputAddresslookup)}`;
-link.textContent = inputAddresslookup;
-nosoAddressValueCell.appendChild(link);
+    // Create a hyperlink to "addresshistory.html" for the NosoAddress value
+    const link = document.createElement('a');
+    link.href = `addresshistory.html?input-field=${encodeURIComponent(inputAddresslookup)}`;
+    link.textContent = inputAddresslookup;
+    nosoAddressValueCell.appendChild(link);
 
-nosoAddressRow.appendChild(nosoAddressPropCell);
-nosoAddressRow.appendChild(nosoAddressValueCell);
-tableBody.appendChild(nosoAddressRow);
+    nosoAddressRow.appendChild(nosoAddressPropCell);
+    nosoAddressRow.appendChild(nosoAddressValueCell);
+    tableBody.appendChild(nosoAddressRow);
 
+    // Hide the spinner once the content is rendered on the page
+    spinner.style.display = 'none';
+  })
+  .catch(error => console.error(error));
 
-      // Hide the spinner once the content is rendered on the page
-      spinner.style.display = 'none';
-    })
-    .catch(error => console.error(error));
 }
+
